71d7be0dda742d2eb3350292c5e5be49a27ba475
Ifeanyichukwu <kamsey11@gmail.com>
2024-04-08 12:29:39 +0100

    remove comments in live chat

--- src/components/ui/LiveChat.jsx
+++ src/components/ui/LiveChat.jsx
@@ -1,8 +1,8 @@
+"use client";
 import React, { useEffect } from "react";
 
 const LiveChat = () => {
   useEffect(() => {
-    // Add the Tawk.to script to the page
     const script = document.createElement("script");
     script.async = true;
     script.src = "https://embed.tawk.to/6613bc011ec1082f04dff168/1hquhcsse";
@@ -10,13 +10,12 @@ const LiveChat = () => {
     script.setAttribute("crossorigin", "*");
     document.body.appendChild(script);
 
-    // Clean up the script when the component is unmounted
     return () => {
       document.body.removeChild(script);
     };
   }, []);
 
-  return null; // The component doesn't render any visible content
+  return null;
 };
 
 export default LiveChat;
